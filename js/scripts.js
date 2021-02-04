4b657a7ab019b04c0a7947a91bffc790cdcf21fa
shanole <73411129+shanole@users.noreply.github.com>
2021-02-03 19:02:42 -0800

    Create function to count unlucky fortune items
    Co-authored-by: Marni Sucher <suchermarni@gmail.com>
    Co-authored-by: Juan Hasbun <zemenarwow@gmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,19 @@
+function countUnlucky(arr) {
+  let numOfUnlucky = 0
+  const unluckyItems = ["scissors", "phone","grief","crying","zoom","politics","travel","drunk","politic","thing"]
+
+  for (i=0; i < arr.length; i+=1) {
+    let userInput = arr[i];
+    if (unluckyItems.includes(userInput)) {
+      numOfUnlucky += 1;
+    }
+    else {
+      continue;
+    }
+  }
+  return numOfUnlucky;
+}
+
 $(document).ready(function() {
   $("form#fortune").submit(function(event) {
     event.preventDefault();
